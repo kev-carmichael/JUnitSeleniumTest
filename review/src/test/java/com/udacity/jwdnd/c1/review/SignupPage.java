4198c237e78e2075d7c278e6b8e4153d0ab9db84
Kev Carmichael <kev.carmichael22@gmail.com>
2021-08-03 17:19:01 +0100

    completed SignupPage.java

--- review/src/test/java/com/udacity/jwdnd/c1/review/SignupPage.java
+++ review/src/test/java/com/udacity/jwdnd/c1/review/SignupPage.java
@@ -1,4 +1,35 @@
 package com.udacity.jwdnd.c1.review;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SignupPage {
+    @FindBy(css = "#inputFirstName")
+    private WebElement firstNameField;
+
+    @FindBy(css = "#inputLastName")
+    private WebElement lastNameField;
+
+    @FindBy(css = "#inputUsername")
+    private WebElement userNameField;
+
+    @FindBy(css = "#inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(css = "#submit-button")
+    private WebElement submitButton;
+
+    public SignupPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void signup(String firstName, String lastName, String username, String password) {
+        this.firstNameField.sendKeys(firstName);
+        this.lastNameField.sendKeys(lastName);
+        this.userNameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+        this.submitButton.click();
+    }
 }
