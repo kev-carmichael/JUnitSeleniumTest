4c9c2c628ef595f5c7e7479222dc2d12721db8e0
Kev Carmichael <kev.carmichael22@gmail.com>
2021-08-04 10:17:27 +0100

    LoginPage.java completed

--- review/src/test/java/com/udacity/jwdnd/c1/review/LoginPage.java
+++ review/src/test/java/com/udacity/jwdnd/c1/review/LoginPage.java
@@ -1,4 +1,28 @@
 package com.udacity.jwdnd.c1.review;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    @FindBy(css = "#inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(css = "#inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(css = "#submit-button")
+    private WebElement submitButton;
+
+    public LoginPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password){
+        this.usernameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+        this.submitButton.click();
+    }
 }
